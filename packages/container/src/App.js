dcce651b611fde344b948d1449ae481ea4f5b661
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-06-06 22:29:46 +0500

    added header in container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
-    return <div>
-        <h1> Hi Micro Frontend!!!</h1>
-        <hr />
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
         <MarketingApp />
-    </div>
+      </div>
+    </BrowserRouter>
+  );
 };
