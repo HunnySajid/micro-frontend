5a50dd5b4081ecb43f9f673d356ba9eeb51525e4
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-06-06 20:14:02 +0500

    fixed typo

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Mfe#1 Home Page
+              Mfe#1 - Home Page
             </Typography>
             <Typography
               variant="h5"
