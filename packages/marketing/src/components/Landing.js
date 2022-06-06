b9c2ba7f4729ee0e0fe5b82ad3c17f8883a67ed5
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-06-06 20:10:43 +0500

    change in marketing to trigger action

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Mfe#1 Home Page
             </Typography>
             <Typography
               variant="h5"
